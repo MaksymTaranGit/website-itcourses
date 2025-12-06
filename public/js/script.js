7ec6836876fb25d7e9faad557291b4bdc2ff168b
Max Taran <maximillian2006.taran@gmail.com>
2025-12-06 14:59:29 +0100

    Add styles with SCSS (7-1 methodology, custom grid + little animation)

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+document.addEventListener('DOMContentLoaded', () => {
+
+    const burgerBtn = document.querySelector('.burger-btn');
+    const navList = document.querySelector('.nav-list');
+
+    if (burgerBtn && navList) {
+        
+        burgerBtn.addEventListener('click', () => {
+            navList.classList.toggle('is-open');
+            const isExpanded = navList.classList.contains('is-open');
+            burgerBtn.setAttribute('aria-expanded', isExpanded);
+        });
+    }
+});
